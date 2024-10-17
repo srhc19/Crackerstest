1a42acbb6315fcd0ea70cd15de0e1ad2f0bee5c4
sarath <srh.c19@gmail.com>
2024-10-17 14:50:30 +0530

    Remove environment files from tracking and update .gitignore

--- src/environments/environment.prod.ts
+++
@@ -1,5 +0,0 @@
-
-export const environment = {
-    production: true,
-    googleRedirectUri: 'http://localhost:4200'
-  };
--- src/environments/environment.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-    production: false,
-    googleRedirectUri: 'http://localhost:4200' ,
-    GOOGLE_CLIENT_ID:  "AIzaSyCjNTYyZLS2x76iWhamrLOrD1O0bOGrFbk"
-  };
